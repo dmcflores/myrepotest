26d6ef35fa6bb70a195e75edc92f215d80555057
dmcflores <dmcflores@chikka.com>
2016-06-30 12:31:39 +0800

    Create puppet.pp

---
+++ puppet.pp
@@ -0,0 +1,49 @@
+node "your hostname here" {
+
+package { 'vim-enhanced':
+        ensure => present,
+}
+package { 'git':
+        ensure => present,
+}
+package { 'curl':
+        ensure => present,
+}
+package { 'wget':
+        ensure => present,
+}
+user { 'monitor':
+        ensure => present,
+        shell => '/bin/bash',
+        home => '/home/monitor',
+        managehome => true,
+}
+file { '/home/monitor/scripts/':
+        ensure => 'directory',
+        owner  => 'monitor',
+        mode   => '0750',
+}
+exec { 'wget':
+        command => '/usr/bin/wget https://raw.githubusercontent.com/dmcflores/myrepotest/master/memory_check -O /home/monitor/scripts/memory_check; chmod +x /home/monitor/scripts/memory_check',
+        creates => '/home/monitor/scripts/memory_check',
+   }
+file { '/home/monitor/src/':
+        ensure => 'directory',
+        owner  => 'monitor',
+        mode   => '0750'
+}
+file { '/home/monitor/src/my_memory_check':
+        ensure => 'link',
+        target => '/home/monitor/scripts/memory_check',
+        owner  => 'monitor',
+        mode   => '0750'
+}
+cron {'cron':
+        command => "/home/monitor/src/my_memory_check",
+        minute => '*/10',
+        hour => '*',
+        month => '*',
+        weekday  => '*',
+        monthday => '*',
+}
+}
